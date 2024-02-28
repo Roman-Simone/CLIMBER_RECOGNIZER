4eb2c99c7cf596e86c783e80df74bab6c4025163
Roman Simone <83060406+Sro552@users.noreply.github.com>
2024-02-28 15:26:05 +0100

    part of Recognition route

---
+++ main.py
@@ -0,0 +1,178 @@
+from PIL import Image, ImageTk
+import tkinter as tk
+import cv2
+from utils import *
+
+
+# Global variable
+dim_x_window = 900
+dim_y_window = 800
+path_img = "Media/2_image_lead.jpg"
+start_x = 0
+start_y = 0
+finish_x = 0
+finish_y = 0
+is_cropped = False
+colorRGB_route = (0, 0, 0)
+colorHSV_route = (0, 0, 0)
+
+# Load the image
+cv_image = cv2.cvtColor(cv2.imread(path_img), cv2.COLOR_BGR2RGB)
+cv_image_backup = cv_image.copy()
+hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2HSV)
+
+
+def button_findRoute(canvas_image,tk_image):
+    global cv_image, colorHSV_route, hsv_image, cv_image_backup
+
+    h = colorHSV_route[0]
+
+    lower_range = (int(h-8), 50, 50) # lower range of red color in HSV
+    upper_range = (int(h+8), 255, 255) # upper range of red color in HSV
+
+    mask = cv2.inRange(hsv_image, lower_range, upper_range)
+    color_image = cv2.bitwise_and(hsv_image, hsv_image, mask=mask)
+
+    filtered_contours, center_points, cv_image = find_holds(color_image, cv_image)
+
+    
+    cv_image = find_route(center_points, cv_image)
+    cv_image_backup = cv_image.copy()
+
+    image_tk = Image.fromarray(cv_image)
+
+    # Converti l'immagine in formato Tkinter
+    PhotoImage = ImageTk.PhotoImage(image_tk)
+    canvas_image.imgref = PhotoImage
+    canvas_image.itemconfig(tk_image, image=PhotoImage)
+
+# Event handlers for the buttons
+def button_cropIMG(canvas_image, tk_image):
+    global start_x, start_y, finish_x, finish_y, is_cropped, cv_image, hsv_image, cv_image_backup
+
+    if not is_cropped:
+
+        # Crop the image with the dimension of rectangle
+        height, width ,_= cv_image.shape
+        new_w, new_h = resize_img(window.winfo_width(), window.winfo_height(), width, height)       
+        cv_image = cv_image[start_y:finish_y, start_x:finish_x]
+
+        # Resize the crop image
+        height, width ,_= cv_image.shape
+        new_w, new_h = resize_img(window.winfo_width(), window.winfo_height(), width, height)
+        cv_image = cv2.resize(cv_image, (new_w, new_h), interpolation=cv2.INTER_LANCZOS4)
+        hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2HSV)
+        cv_image_backup = cv_image.copy()
+    
+        # Set the image on the canvas
+        tk_image1 = Image.fromarray(cv_image)
+        photo_image = ImageTk.PhotoImage(tk_image1)
+        canvas_image.config(width=new_w, height=new_h)
+        canvas_image.imgref = photo_image
+        canvas_image.itemconfig(tk_image, image=photo_image)
+        canvas_image.delete("rectangle")
+        is_cropped = True
+
+def update_rectangle(event, canvas_image):
+    global start_x, start_y, finish_x, finish_y, is_cropped
+    if not is_cropped:
+        finish_x = event.x
+        finish_y = event.y
+        canvas_image.delete("rectangle")
+        canvas_image.create_rectangle(start_x, start_y, finish_x, finish_y, outline="red", tags="rectangle")
+
+# Funzione per ottenere il colore al click sull'immagine
+def get_color(event, square_canvas, hsv_image):
+    global start_x, start_y, is_cropped, colorRGB_route, colorHSV_route, cv_image
+
+    if not is_cropped:
+        start_x = event.x
+        start_y = event.y
+    else:
+        x, y = event.x, event.y
+        r, g, b = cv_image[y, x]
+        h, s, v = hsv_image[y, x]
+        # Crea un quadrato dello stesso colore
+        colorRGB_route = (r, g, b)
+        colorHSV_route = (h, s, v)
+        square_size = 100
+        square_image = Image.new('RGB', (square_size, square_size), colorRGB_route)
+        square_photo = ImageTk.PhotoImage(square_image)
+        square_canvas.create_image(0, 0, image=square_photo, anchor=tk.NW)
+        square_canvas.image = square_photo
+
+def on_resize(event, canvas_image, tk_image):
+    # Questa funzione viene chiamata quando la finestra viene ridimensionata
+    global cv_image, hsv_image, cv_image_backup
+    
+    height, width ,_= cv_image.shape
+    new_w, new_h = resize_img(window.winfo_width(), window.winfo_height(), width, height)
+    cv_image = cv2.resize(cv_image_backup, (new_w, new_h), interpolation=cv2.INTER_LANCZOS4)
+    hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2HSV)
+    tk_image1 = Image.fromarray(cv_image)
+
+    # Converti l'immagine in formato Tkinter
+    PhotoImage = ImageTk.PhotoImage(tk_image1)
+
+    # Aggiorna l'immagine nel canvas
+    canvas_image.config(width=new_w, height=new_h)
+    canvas_image.imgref = PhotoImage
+
+    canvas_image.itemconfig(tk_image, image=PhotoImage)
+
+
+def init_window(window):
+
+    # set title and dimension window
+    window.title("CLIMBER RECOGNIZER")
+    window.geometry(f"{dim_x_window}x{dim_y_window}")
+    
+    # Create a frame and a canvas for the image
+    image_tk = Image.fromarray(cv_image)
+    photo_image = ImageTk.PhotoImage(image_tk)
+    image_frame = tk.Frame(window)
+    image_frame.pack(side=tk.LEFT)
+    canvas_image = tk.Canvas(image_frame)
+    canvas_image.pack()
+    tk_image = canvas_image.create_image(0, 0, image=photo_image, anchor=tk.NW)
+    # Set call-back function for the canvas
+    # canvas_image.bind('<Configure>', lambda event: on_resize(event, canvas_image, tk_image))
+    canvas_image.bind("<Button-1>", lambda event: get_color(event, square_canvas, hsv_image))
+    canvas_image.bind("<B1-Motion>", lambda event: update_rectangle(event, canvas_image))
+    window.bind("<Configure>", lambda event: on_resize(event, canvas_image, tk_image))
+
+
+    # Create a frame for buttons and labels
+    frame_btn = tk.Frame(window)
+    frame_btn.pack()
+    label_title = tk.Label(frame_btn, text="ROUTE RECOGNIZER", font=("Arial", 20, "bold"))
+    label_title.pack()
+
+    # Button to crop the image
+    button1 = tk.Button(frame_btn, text="CROP IMAGE", command=lambda: button_cropIMG(canvas_image, tk_image))
+    button1.pack()
+
+    label_btn1 = tk.Label(frame_btn, text="Color: ")
+    label_btn1.pack()
+
+    # Button and canvas for the color hold
+    label_btn2 = tk.Label(frame_btn, text="Click the image to choose the color hold:", font=("Arial", 12))
+    label_btn2.pack()
+    square_canvas = tk.Canvas(frame_btn, width=100, height=100)
+    colorRGB_route = (149, 20, 6)
+    square_size = 100
+    square_image = Image.new('RGB', (square_size, square_size), colorRGB_route)
+    square_photo = ImageTk.PhotoImage(square_image)
+    square_canvas.create_image(0, 0, image=square_photo, anchor=tk.NW)
+    square_canvas.image = square_photo
+    square_canvas.pack()
+    button2 = tk.Button(frame_btn, text="DRAW THE LINE", command=lambda: button_findRoute(canvas_image, tk_image))
+    button2.pack()
+
+
+if __name__ == "__main__":
+    # Create window
+    window = tk.Tk()
+    init_window(window)
+    
+    window.mainloop()
--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import os
 from skimage.restoration import denoise_tv_bregman
 from skimage.morphology import disk
 from skimage import filters
+import numpy as np
 
 # Variabili globali
 ix, iy = -1, -1
@@ -85,27 +86,92 @@ def find_countour(img):
     return result_img, filtered_contours
 
 
+def find_holds(color_image, cv_image):
+    # Apply Canny edge detection and find contours
+    edges = cv2.Canny(color_image, 50, 150)
+    kernel = np.ones((3, 3), np.uint8)      # Dilate the edges to improve contour detection
+    dilated_edges = cv2.dilate(edges, kernel, iterations=1)
+    contours, _ = cv2.findContours(dilated_edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 
+    # Filter contours by area and non-hollow contours
+    filtered_contours = []
+    center_points = []
+    for cnt in contours:
+        if cv2.contourArea(cnt) > 50:
+            filtered_contours.append(cnt)
+            center = cv2.moments(cnt)
+            cx = int(center["m10"] / center["m00"])
+            cy = int(center["m01"] / center["m00"])
+            center_points.append((cx, cy))
 
+    center_points = sorted(center_points, key=lambda point: point[1])
+    coppie = [(0,0)]
+    for i in range(len(center_points)):
+        # Find the nearest neighbor for each point
+        nearest_neighbor = None
+        min_distance = 10000000
+        for j in range(len(center_points)):
+            if i != j and (i, j) not in coppie and (j, i) not in coppie and center_points[i][1] < center_points[j][1]:
+                distance = np.sqrt((center_points[i][0] - center_points[j][0])**2 + (center_points[i][1] - center_points[j][1])**2)
+                if distance < min_distance:
+                    min_distance = distance
+                    nearest_neighbor = j
+        
+        # Connect the points with a line
+        if nearest_neighbor is not None:
+            coppie.append((i, nearest_neighbor))
+            cv2.line(cv_image, center_points[i], center_points[nearest_neighbor], (0, 0, 255), 2)
+    # Draw contours on the color image
+    cv2.drawContours(cv_image, filtered_contours, -1, (0, 255, 0), 2)
 
+    return filtered_contours, center_points, cv_image
 
+def find_route(center_points, cv_image):
+    # Sort center points from lowest to highest
+    mean_points = []
+    temp = []
+    for i in range(len(center_points)):
+        temp.append(center_points[i])
+        intermediate_centers = [point for point in temp]
+        avg_center_x = sum([point[0] for point in intermediate_centers]) / len(intermediate_centers)
+        avg_center_y = sum([point[1] for point in intermediate_centers]) / len(intermediate_centers)
+        mean_points.append((int(avg_center_x), int(avg_center_y)))
+    
+    center_points = sorted(center_points, key=lambda point: point[1], reverse=True)
+    temp = []
+    for i in range(len(center_points)):
+        temp.append(center_points[i])
+        intermediate_centers = [point for point in temp]
+        avg_center_x = sum([point[0] for point in intermediate_centers]) / len(intermediate_centers)
+        avg_center_y = sum([point[1] for point in intermediate_centers]) / len(intermediate_centers)
+        mean_points.append((int(avg_center_x), int(avg_center_y)))
+    
+    mean_points = sorted(mean_points, key=lambda point: point[1])
+    
+    for i in range(len(mean_points) - 1):
+        cv_image = cv2.circle(cv_image, (mean_points[i][0], mean_points[i][1]), 5, (255, 0, 0), -1)
+        cv_image = cv2.line(cv_image, (int(mean_points[i][0]), int(mean_points[i][1])), (int(mean_points[i+1][0]), int(mean_points[i+1][1])), (0, 255, 255), 2)
 
+    return cv_image
 
+def resize_img(win_width, win_height, width, height):
+    win_width = win_width-230
+    win_height = win_height
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if width > height or width > win_width:
+        scalingfactor = win_width/width
+        new_width = win_width
+        new_height = int(height*scalingfactor)
+        print("\n----DEBUG A----")
+    else:
+        print("\n----DEBUG B----")
+        scalingfactor = win_height/height
+        new_height = win_height
+        new_width = int(width*scalingfactor)
+        if new_width > win_width:
+            new_width = width
+            new_height = height
+            
+    return new_width, new_height
 
